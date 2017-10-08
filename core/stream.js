439ed3c3db8a01f1d7706477e835b99bce549a87
Phoomparin Mano <work@phoom.in.th>
2017-10-09 00:09:14 +0700

    :hammer: Refactored Encoder Code

--- core/stream.js
+++ core/stream.js
@@ -3,6 +3,7 @@ const ffmpeg = require('fluent-ffmpeg')
 
 const {fps, logFile, size} = require('../config')
 
+// Custom FFMPEG Flags
 // prettier-ignore
 const customFlags = [
   '-threads', '0',  // Threads: 0
@@ -10,7 +11,42 @@ const customFlags = [
   '-v', 'verbose',  // Logging Level: Verbose
 ]
 
-// prettier-ignore
+// FMPEG Encoder
+function Encoder(input, output) {
+  // Video Input
+  // Codec: PNG
+  // Format: Image to Pipe
+  const vInput = ffmpeg(input)
+    .inputOptions(customFlags)
+    .inputOptions('-c:v', 'png')
+    .inputFPS(fps)
+    .inputFormat('image2pipe')
+
+  // Audio Settings
+  // Source: Null Audio Source
+  // Input Format: LibAV Virtual Input
+  const vAudio = vInput
+    .input('anullsrc')
+    .inputFormat('lavfi')
+    .audioCodec('aac')
+    .audioChannels(1)
+    .audioFrequency(44100)
+    .audioBitrate(128)
+
+  // Video Output
+  // Codec: H264 (libx264)
+  // Output: RTMP Stream to Facebook Live
+  // Pix Format: YUV420P
+  const vOutput = vAudio
+    .addOption('-c:v', 'libx264')
+    .size(`${size.width}x${size.height}`)
+    .addOption('-pix_fmt', 'yuv420p')
+    .format('flv')
+    .output(output)
+
+  return vOutput
+}
+
 class VideoStream extends Writable {
   constructor(rtmpUrl) {
     super()
@@ -18,44 +54,12 @@ class VideoStream extends Writable {
     // Input Stream
     this.input = new PassThrough()
 
-    // Video Input
-    // Codec: PNG
-    // Format: Image to Pipe
-    const input = ffmpeg({logger: console})
-      .input(this.input)
-      .inputOptions(customFlags)
-      .inputOptions('-c:v', 'png')
-      .inputFPS(fps)
-      .inputFormat('image2pipe')
-
-    // Audio Settings
-    // Source: Null Audio Source
-    // Input Format: LibAV Virtual Input
-    const audio = input
-      .input('anullsrc')
-      .inputFormat('lavfi')
-      .audioCodec('aac')
-      .audioChannels(1)
-      .audioFrequency(44100)
-      .audioBitrate(128)
-
-    // Video Output
-    // Codec: H264 (libx264)
-    // Output: RTMP Stream to Facebook Live
-    // Pix Format: YUV420P
-    const output = audio
-      .addOption('-c:v', 'libx264')
-      .size(`${size.width}x${size.height}`)
-      .addOption('-pix_fmt', 'yuv420p')
-      .format('flv')
-      .output(rtmpUrl)
-
-    const instance = output
+    // FFMPEG Encoder Instance
+    const encoder = Encoder(this.input, rtmpUrl)
       .on('data', data => console.info('On Data', data))
       .on('error', err => console.warn('On Error', err))
       .on('end', () => console.info('On End', data))
-
-    instance.run()
+      .run()
   }
 
   write(buffer) {
