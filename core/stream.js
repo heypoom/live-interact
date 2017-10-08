65467654bac5af63573e4333fa340a980d15c0f5
Phoomparin Mano <work@phoom.in.th>
2017-10-08 23:48:02 +0700

    :sparkles: Used Fluent FFMPEG instead of manually using flags

--- core/stream.js
+++ core/stream.js
@@ -3,78 +3,67 @@ const {Writable, PassThrough} = require('stream')
 const {spawn} = require('child_process')
 const ffmpeg = require('fluent-ffmpeg')
 
-const {fps, logFile, size: {width, height}} = require('../config')
+const {fps, logFile, size} = require('../config')
 
 const logStream = fs.createWriteStream(logFile)
 
 // prettier-ignore
-const ffmpegFlags = [
-  '-threads', '0',
-  '-y',
-  '-v', 'verbose',
-
-  '-c:v', 'png',
-  '-r', fps,
-  '-f', 'image2pipe',
-  '-i', '-',
-  '-f', 'lavfi',
-  '-i', 'anullsrc',
-
-  '-acodec', 'aac',
-  '-ac', '1',
-  '-ar', '44100',
-  '-b:a', '128k',
-  '-c:v', 'libx264',
-  '-s', `${width}x${height}`,
-  '-pix_fmt', 'yuv420p',
-  '-f', 'flv'
+const customFlags = [
+  '-threads', '0',  // Threads: 0
+  '-y',             // Replace Existing File
+  '-v', 'verbose',  // Logging Level: Verbose
 ]
 
-// .input('-', this.input)
-// .fps(fps)
-// .inputFormat('image2pipe')
-// .inputFormat('lavfi')
-// .videoCodec('png')
-// .input('anullsrc')
-// .audioCodec('aac')
-// .audioChannels(1)
-// .audioFrequency(44100)
-// .audioBitrate(128)
-// .videoCodec('libx264')
-// .size('1280x720')
-// .outputOptions('-pix_fmt', 'yuv420p')
-// .format('flv')
-
-// this.input = new PassThrough()
-
-// const command = ffmpeg(this.input)
-//   .format('flv')
-//   .size('1280x720')
-//   .videoCodec('libx264')
-//   .fps(fps)
-//   .audioBitrate(128)
-//   .audioCodec('aac')
-//   .audioFrequency(44100)
-//   .audioChannels(2)
-//   .output(rtmpUrl)
-//   .on('error', err => console.error('FFMPEG Error:', err))
-//   .on('data', data => console.info('FFMPEG Data:', data))
-//   .on('end', () => console.info('Finished Processing...'))
-//
-// console.info('ffmpeg', command._getArguments().join(' '))
-//
-// command.run()
-
+// prettier-ignore
 class VideoStream extends Writable {
   constructor(rtmpUrl) {
     super()
-    const args = [...ffmpegFlags, rtmpUrl]
-    const stdio = ['pipe', 1, 2, 'ipc']
-    this.ffmpeg = spawn('ffmpeg', args, {stdio})
+
+    // Input Stream
+    this.input = new PassThrough()
+
+    // Video Input
+    // Codec: PNG
+    // Format: Image to Pipe
+    const input = ffmpeg({logger: console})
+      .input(this.input)
+      .inputOptions(customFlags)
+      .inputOptions('-c:v', 'png')
+      .inputFPS(fps)
+      .inputFormat('image2pipe')
+
+    // Audio Settings
+    // Source: Null Audio Source
+    // Input Format: LibAV Virtual Input
+    const audio = input
+      .input('anullsrc')
+      .inputFormat('lavfi')
+      .audioCodec('aac')
+      .audioChannels(1)
+      .audioFrequency(44100)
+      .audioBitrate(128)
+
+    // Video Output
+    // Codec: H264 (libx264)
+    // Output: RTMP Stream to Facebook Live
+    // Pix Format: YUV420P
+    const output = audio
+      .addOption('-c:v', 'libx264')
+      .size(`${size.width}x${size.height}`)
+      .addOption('-pix_fmt', 'yuv420p')
+      .format('flv')
+      .output(rtmpUrl)
+
+    const instance = output
+      .on('data', data => console.info('On Data', data))
+      .on('error', err => console.warn('On Error', err))
+      .on('end', () => console.info('On End', data))
+
+    instance.run()
   }
 
   write(buffer) {
-    this.ffmpeg.stdin.write(buffer)
+    this.input.write(buffer)
   }
 }
 
